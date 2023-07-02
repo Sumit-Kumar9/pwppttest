302ca45b7a5e34c7d2626e8cafc0f950616866cb
Sumit Kumar <127492628+Sumit-Kumar9@users.noreply.github.com>
2023-07-03 00:14:44 +0530

    Add files via upload

---
+++ declare.java
@@ -0,0 +1,12 @@
+//Write a Java program to declare and initialize variables of different data types (int, double, String) with appropriate identifiers.
+public class declare {
+    public static void main(String[] args) {
+        int age = 18;
+        double salery = 50000.713;
+        String name = "Sumit Kumar";
+        System.out.println("age :"+age);
+        System.out.println("Salery :"+salery);
+        System.out.println("Name: "+name);
+
+    }
+}
